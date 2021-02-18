0b6c5a44577fcc27fc72d756fd60669f6003a865
aijazraja <bazaaraijaz@gmail.com>
2021-02-18 15:26:34 +0100

    nodejs week3 is ready for feedback

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -4,10 +4,106 @@ const knex = require("../database");
 
 router.get("/", async (request, response) => {
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meals").select("title");
-    response.json(titles);
+    if(request.query.maxPrice){
+      const maxPrice=parseInt(request.query.maxPrice);
+      const mealsWithMaxPrice=await knex("meals").where("price", "<", maxPrice) ;
+      response.json(mealsWithMaxPrice);
+      } 
+    else if(request.query.tittle){
+     const givenTittle=request.query.tittle;
+     const mealsWithTittle=await knex('meals')
+                           .where("tittle",'like', '%'+givenTittle+'%') ;
+     response.json(mealsWithTittle);                     
+
+     }
+    else if(request.query.limit){
+      const givenLimit=parseInt(request.query.limit);
+      const mealsWithLimit=await knex('meals')
+                          .limit(givenLimit);
+                          
+      response.json(mealsWithLimit);  
+
+     }
+     else if(request.query.availableReservations){
+      const mealsWithReservations=await knex('meals')
+                          .where("max_reservations", ">", 0) ;
+                          
+      response.json(mealsWithReservations);  
+     }
+     else if(request.query.createdAfter){
+      const givenDate=new Date(request.query.createdAfter);
+      const mealsCreatedAfterDate=await knex('meals').where('created_date','>',givenDate);
+                          
+      response.json(mealsCreatedAfterDate);  
+     }
+   else{
+      const titles = await knex("meals").select("tittle");
+      const meals = await knex("meals");
+      response.json(meals);
+    }
   } catch (error) {
+    throw error;
+  }
+});
+
+
+
+router.post("/", async (request, response) => {
+  try {
+ 
+    const newMeal=await knex('meals')
+                      .insert({
+                      tittle: 'lahori karahi',
+                      description: 'lahori karahi from Pakistan',
+                      location: 'Odense',
+                      when: new Date(),
+                      max_reservations: 7,
+                      price: 190,
+                      created_date: new Date(),
+
+    });
+    response.json(newMeal);
+  } catch (error) {
+    throw error;
+  }
+});
+
+
+
+router.get("/:id", async (request, response) => {
+  try {
+    const mealByid = await knex("meals")
+                    .where({id:parseInt(request.params.id)});
+    response.json(mealByid);
+  } catch (error) {
+    throw error;
+  }
+});
+
+
+
+router.put("/:id", async (request, response)=>{
+try {
+  const updatedMeal=await knex("meals")
+                    .where({id:parseInt(request.params.id)})
+                    .update({tittle:'italian pasta',
+                             description:'yummy italian pasta',});
+   response.json(updatedMeal);
+}catch(error){
+  throw error;
+}
+});
+
+
+
+router.delete("/:id", async (request, response)=>{
+  try{
+    const deletedMeal=await knex("meals")
+                      .where({id:parseInt(request.params.id)})
+                      .del();
+    response.json(deletedMeal);
+
+  }catch(error){
     throw error;
   }
 });
